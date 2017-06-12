045522035d086ef4ee2b94fa70f84dc2e8a5bf1c
Florin <oancea.quentin.florin@gmail.com>
2017-06-12 16:18:50 +0300

    updating the MOngoDb settings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://florin:florin0@ds157078.mlab.com:57078/todoapp" || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
